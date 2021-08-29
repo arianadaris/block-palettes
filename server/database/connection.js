92840f220fe2284e4b6c450076f7d58adcf56c2a
Ariana <73635827+arianadaris@users.noreply.github.com>
2021-08-29 00:59:33 -0700

    Create connection.js

---
+++ server/database/connection.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () =>
+{
+    try
+    {
+        const con = await mongoose.connect(process.env.DATABASE_URL, 
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+                useCreateIndex: true
+            });
+
+        console.log(`MongoDB connected: ${con.connection.host}`);
+    }
+    catch (err)
+    {
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
